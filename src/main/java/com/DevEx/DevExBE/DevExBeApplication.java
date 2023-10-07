0c5809ed629819cf28b062ccf919564ed2f1dbdd
KongJunWoo <joonoo3@naver.com>
2023-10-07 14:47:37 +0900

    init

---
+++ src/main/java/com/DevEx/DevExBE/DevExBeApplication.java
@@ -0,0 +1,13 @@
+package com.DevEx.DevExBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevExBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevExBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/DevEx/DevExBE/DevExBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DevEx.DevExBE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevExBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
