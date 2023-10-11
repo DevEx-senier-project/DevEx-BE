3d9f3b5cbf2680196ff7a468e9817255017e0d92
KongJunWoo <joonoo3@naver.com>
2023-10-12 01:24:16 +0900

    feat : Fedex quote 받아오는 기능 추가
    - AOP 측정 결과 토큰 받아오는 이슈로 수행 시간 7s (개선필요)
    - TestRequest 객체 생성시 사용자의 request 값에 따라 result값 바꾸어줘야함

--- src/main/java/com/DevEx/DevExBE/API/Contrller/FedexController.java
+++ src/main/java/com/DevEx/DevExBE/API/Contrller/FedexController.java
@@ -1,20 +1,28 @@
 package com.DevEx.DevExBE.API.Contrller;
 
+import com.DevEx.DevExBE.API.Entity.Fedex.TestRequest;
 import com.DevEx.DevExBE.API.FedexService;
+import com.google.gson.JsonObject;
 import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@AllArgsConstructor
+@RequiredArgsConstructor
 @RequestMapping("/api/fedex")
 public class FedexController {
 
         private final FedexService fedexService;
 
         @GetMapping("/token")
-        public String getFedexTest() {
+        public String getFedexToken() {
             return fedexService.getFedexToken();
+        }
+
+        @GetMapping("/quote")
+        public JsonObject getFedexQuote() {
+            return fedexService.postFedexQuote();
         }
 }
--- src/main/java/com/DevEx/DevExBE/API/FedexService.java
+++ src/main/java/com/DevEx/DevExBE/API/FedexService.java
@@ -1,6 +1,10 @@
 package com.DevEx.DevExBE.API;
 
+import com.DevEx.DevExBE.API.Entity.Fedex.TestRequest;
 import com.DevEx.DevExBE.API.Entity.Fedex.Token;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
@@ -11,15 +15,16 @@ import reactor.core.publisher.Mono;
 @Service
 public class FedexService {
 
-    WebClient dhlWebClient = WebClient.builder()
-            .baseUrl("https://apis.fedex.com")
-            .build();
     @Value("${API.Fedex.grant_type}")
     private String grant_type;
     @Value("${API.Fedex.client_id}")
     private String client_id;
     @Value("${API.Fedex.client_secret}")
     private String client_secret;
+
+    WebClient dhlWebClient = WebClient.builder()
+            .baseUrl("https://apis.fedex.com")
+            .build();
 
     /***
      * Fedex API를 통해 access토큰을 받아온다.
@@ -37,5 +42,25 @@ public class FedexService {
                 .bodyToMono(Token.class);
 
         return testClient.block().getAccess_token();
+    }
+
+    public JsonObject postFedexQuote() {
+
+         TestRequest testRequest = new TestRequest();
+
+        String testClient = dhlWebClient.post()
+                .uri("/rate/v1/rates/quotes")
+                .contentType(MediaType.APPLICATION_JSON)
+                .header("authorization", "Bearer " + getFedexToken())
+                .bodyValue(testRequest.getResult())
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+
+        System.out.println(testClient.toString());
+        JsonParser parser = new JsonParser();
+        JsonObject jsonObject = (JsonObject) parser.parse(testClient);
+
+        return jsonObject;
     }
 }
