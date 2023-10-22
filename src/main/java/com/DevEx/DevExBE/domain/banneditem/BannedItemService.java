f7fb6440f6f217263203796aa3021b3b11146058
KongJunWoo <joonoo3@naver.com>
2023-10-23 01:02:33 +0900

    chore  : BannedItemService에서 todo 작성하였습니다.

--- src/main/java/com/DevEx/DevExBE/domain/banneditem/BannedItemService.java
+++ src/main/java/com/DevEx/DevExBE/domain/banneditem/BannedItemService.java
@@ -7,10 +7,12 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+// TODO: 2023-10-23 1. banneditem 수정 기능 추가
 @RequiredArgsConstructor
 @Service
 public class BannedItemService {
     private final BannedItemRepository bannedItemRepository;
+
 
     public void addBannedItem(List<Item> ItemList, Handcarry handcarry) {
         ItemList.stream().map(item -> bannedItemRepository.save(BannedItem.toEntity(item, handcarry)));
