3f2f898c8d7f4691fb36bc3e2c36c74e60f86ed1
KongJunWoo <joonoo3@naver.com>
2023-10-31 21:12:15 +0900

    style : 가독성 정리

--- src/main/java/com/DevEx/DevExBE/domain/handcarry/dto/HandcarryResponseDto.java
+++ src/main/java/com/DevEx/DevExBE/domain/handcarry/dto/HandcarryResponseDto.java
@@ -2,14 +2,22 @@ package com.DevEx.DevExBE.domain.handcarry.dto;
 
 import lombok.*;
 
+import java.util.List;
+
 @Getter
 @Setter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class HandcarryResponseDto {
+
     private String startPoint;
+
     private String endPoint;
+
     private Long unitCosts;
+
     private Long maxWeight;
+
+    private List<String> bannedItemList;
 }
