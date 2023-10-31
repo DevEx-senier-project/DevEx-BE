b6dfbc743d942619421b1e218a722c3cde62fce4
KongJunWoo <joonoo3@naver.com>
2023-10-31 21:14:31 +0900

    feat : addBannedItem 작동 원리 변경
    - String으로 변수형이 변경됨에 따라 수정되었습니다.
    - Item 리스트를 돌며 Item 객체를 찾아 해당 handcarry 업체와 함께 BannedItem 객체에 생성되어 저장됩니다.

--- src/main/java/com/DevEx/DevExBE/domain/banneditem/BannedItemService.java
+++ src/main/java/com/DevEx/DevExBE/domain/banneditem/BannedItemService.java
@@ -2,6 +2,7 @@ package com.DevEx.DevExBE.domain.banneditem;
 
 import com.DevEx.DevExBE.domain.handcarry.Handcarry;
 import com.DevEx.DevExBE.domain.item.Item;
+import com.DevEx.DevExBE.domain.item.ItemRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -12,9 +13,16 @@ import java.util.List;
 @Service
 public class BannedItemService {
     private final BannedItemRepository bannedItemRepository;
+    private final ItemRepository itemRepository;
 
 
-    public void addBannedItem(List<Item> ItemList, Handcarry handcarry) {
-        ItemList.stream().map(item -> bannedItemRepository.save(BannedItem.toEntity(item, handcarry)));
+    //itemNames으로 Item 찾아서 bannedItem에 저장
+    public void addBannedItem(List<String> ItemList, Handcarry handcarry) {
+        ItemList.stream().map(item -> {
+                    Item itemEntity = itemRepository.findByName(item);
+                    bannedItemRepository.save(BannedItem.toEntity(itemEntity, handcarry));
+                    return itemEntity;
+                }
+        );
     }
 }
