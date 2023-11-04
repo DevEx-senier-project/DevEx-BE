a2545d0774a7a84ec213673ebecd813f0ddd4266
tjdgns8439 <tjdgns8439@inha.edu>
2023-11-05 02:26:49 +0900

    feat: Update WebSecurityConfig
    - csrf, session기반 해제
    - /auth/**(login, signup)는 인증없이 허용
    - custom한 에러핸들러적용

--- src/main/java/com/DevEx/DevExBE/global/security/WebSecurityConfig.java
+++ src/main/java/com/DevEx/DevExBE/global/security/WebSecurityConfig.java
@@ -1,67 +1,56 @@
 package com.DevEx.DevExBE.global.security;
 
+import com.DevEx.DevExBE.global.jwt.JwtAccessDeniedHandler;
+import com.DevEx.DevExBE.global.jwt.JwtAuthenticationEntryPoint;
+import com.DevEx.DevExBE.global.jwt.JwtProvider;
+import com.DevEx.DevExBE.global.jwt.JwtSecurityConfig;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
-import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-
-import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
-
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.filter.CorsFilter;
 
 @RequiredArgsConstructor
 @Configuration
 @EnableWebSecurity
-public class WebSecurityConfig {
+public class WebSecurityConfig{
+
+    private UserDetailsServiceImpl userDetailsService;
+    private final JwtProvider jwtProvider;
+    private final CorsFilter corsFilter;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+
+
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain apiFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
-                .formLogin(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(authorizeRequest ->
-                        authorizeRequest
-                                .requestMatchers(
-                                        antMatcher("/auth/**")
-                                ).authenticated()
-                                .requestMatchers(
-                                        antMatcher("/h2-console/**")
-                                ).permitAll()
-                )
-                .headers(
-                        headersConfigurer ->
-                                headersConfigurer
-                                        .frameOptions(
-                                                HeadersConfigurer.FrameOptionsConfig::sameOrigin
-                                        )
-                                        .contentSecurityPolicy( policyConfig ->
-                                                policyConfig.policyDirectives(
-                                                        "script-src 'self'; " + "img-src 'self'; " +
-                                                                "font-src 'self' data:; " + "default-src 'self'; " +
-                                                                "frame-src 'self'"
-                                                )
-                                        )
-                );
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(corsFilter, UsernamePasswordAuthenticationFilter.class)
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(new AntPathRequestMatcher("/auth/**")).permitAll()
+                        .anyRequest().authenticated())
 
+                .exceptionHandling(exceptionHandling -> exceptionHandling
+                        .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                        .accessDeniedHandler(jwtAccessDeniedHandler))
+
+                // JwtFilter 를 addFilterBefore 로 등록했던 JwtSecurityConfig 클래스를 적용
+                .apply(new JwtSecurityConfig(jwtProvider));
         return http.build();
     }
 
-    @Bean
-    public AuthenticationManager authenticationManager(HttpSecurity http, BCryptPasswordEncoder bCryptPasswordEncoder,
-                                                       UserDetailsService userDetailsService) throws Exception{
-        return http.getSharedObject(AuthenticationManagerBuilder.class)
-                .userDetailsService(userDetailsService)
-                .passwordEncoder(bCryptPasswordEncoder)
-                .and().build();
-    }
 
+    //bcrypt 강력 해싱 함수로 암호 인코딩
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder(){
         return new BCryptPasswordEncoder();
