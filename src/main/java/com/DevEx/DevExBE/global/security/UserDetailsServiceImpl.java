2d60be951e2ca98e6f304600e4fc155df372c49b
tjdgns8439 <tjdgns8439@inha.edu>
2023-11-05 02:27:54 +0900

    feat: Update UserDetailsServiceImpl
    - UserDetailServices 구현체

--- src/main/java/com/DevEx/DevExBE/global/security/UserDetailsServiceImpl.java
+++ src/main/java/com/DevEx/DevExBE/global/security/UserDetailsServiceImpl.java
@@ -3,7 +3,9 @@ package com.DevEx.DevExBE.global.security;
 import com.DevEx.DevExBE.domain.users.UserRepository;
 import com.DevEx.DevExBE.domain.users.Users;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
@@ -11,9 +13,12 @@ import org.springframework.stereotype.Service;
 public class UserDetailsServiceImpl implements UserDetailsService {
     private final UserRepository userRepository;
 
+    //주어진 email을 가진 사용자의 세부 정보를 얻는다.
+    //이메일은 고유하다고 간주한다.
+    //사용자 이름이 존재하지 않으면 메서드가 UserNameNotFoundException을 투척한다.
     @Override
-    public Users loadUserByUsername(String email){
-        return userRepository.findByEmail(email)
-                .orElseThrow(() -> new IllegalArgumentException(email));
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Users user = userRepository.findByEmail(email).orElseThrow(() -> new UsernameNotFoundException("user not found"));
+        return new UserDetailsImpl(user);
     }
 }
