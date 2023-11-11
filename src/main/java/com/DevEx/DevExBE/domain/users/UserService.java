8d3539fdfd890aab0339abe5c0edd72ff26c6024
KongJunWoo <joonoo3@naver.com>
2023-11-12 01:12:12 +0900

    feat : 회원가입 메소드 중복 삭제 AuthService와 UserService에 중복으로 있어서 삭제했습니다.

--- src/main/java/com/DevEx/DevExBE/domain/users/UserService.java
+++ src/main/java/com/DevEx/DevExBE/domain/users/UserService.java
@@ -1,11 +1,9 @@
 package com.DevEx.DevExBE.domain.users;
 
 import com.DevEx.DevExBE.domain.corporation.CorporationRepository;
-import com.DevEx.DevExBE.domain.users.dto.AddUserRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -16,35 +14,22 @@ import java.util.Optional;
 public class UserService {
     private final UserRepository userRepository;
     private final CorporationRepository corporationRepository;
-    private BCryptPasswordEncoder bCryptPasswordEncoder;
 
-
-    /*
-    //   일반 회원가입
-    */
-    // TODO: 2023/10/22 User -> Corporation 간 양방향 매핑
-    public Users signUp(AddUserRequestDto requestDto){
-        return userRepository.save(Users.builder()
-                .email(requestDto.getEmail())
-                .password(bCryptPasswordEncoder.encode(requestDto.getPassword()))
-                .build());
-    }
-
-    public List<Users> getUserList(){
+    public List<Users> getUserList() {
         return userRepository.findAll();
     }
 
-    public Optional<Users> getUserId(Long id){
+    public Optional<Users> getUserId(Long id) {
         return userRepository.findById(id);
     }
 
-    public ResponseEntity<Void> deleteUser(Long id){
+    public ResponseEntity<Void> deleteUser(Long id) {
         userRepository.deleteById(id);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
     // email을 통해 유저 정보를 가져온다.
-    public Users getUserByEmail(String email){
+    public Users getUserByEmail(String email) {
         return userRepository.findByEmail(email).orElseThrow(
                 () -> new IllegalArgumentException("해당 유저가 없습니다.")
         );
