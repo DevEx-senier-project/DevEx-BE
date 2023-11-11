1f9574a45090edf95ec5d3840fc6df6afa094074
KongJunWoo <joonoo3@naver.com>
2023-11-12 04:24:23 +0900

    feat : bannedItem 저장 기능 추가

--- src/main/java/com/DevEx/DevExBE/domain/banneditem/BannedItemService.java
+++ src/main/java/com/DevEx/DevExBE/domain/banneditem/BannedItemService.java
@@ -3,9 +3,11 @@ package com.DevEx.DevExBE.domain.banneditem;
 import com.DevEx.DevExBE.domain.handcarry.Handcarry;
 import com.DevEx.DevExBE.domain.item.Item;
 import com.DevEx.DevExBE.domain.item.ItemRepository;
+import com.DevEx.DevExBE.domain.item.ItemService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 // TODO: 2023-10-23 1. banneditem 수정 기능 추가
@@ -14,15 +16,25 @@ import java.util.List;
 public class BannedItemService {
     private final BannedItemRepository bannedItemRepository;
     private final ItemRepository itemRepository;
+    private final ItemService itemService;
 
 
     //itemNames으로 Item 찾아서 bannedItem에 저장
     public void addBannedItem(List<String> ItemList, Handcarry handcarry) {
-        ItemList.stream().map(item -> {
-                    Item itemEntity = itemRepository.findByItemName(item);
-                    bannedItemRepository.save(BannedItem.toEntity(itemEntity, handcarry));
-                    return itemEntity;
-                }
-        );
+
+        try{
+
+            List<Item> itemList = itemService.getItemList(ItemList);
+
+            for (Item item : itemList) {
+                bannedItemRepository.save(BannedItem.toEntity(item, handcarry));
+            }
+
+        }
+        catch (Exception e){
+            System.out.println("error");
+        }
+
     }
+
 }
