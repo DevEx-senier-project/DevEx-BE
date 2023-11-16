f61d3bbc073b0c038e9c0dc7725a694b76590d88
tjdgns8439 <tjdgns8439@inha.edu>
2023-11-16 17:25:09 +0900

    bug: cors error 해결 테스트

--- src/main/java/com/DevEx/DevExBE/global/security/WebConfig.java
+++ src/main/java/com/DevEx/DevExBE/global/security/WebConfig.java
@@ -1,17 +1,17 @@
-//package com.DevEx.DevExBE.global.security;
-//
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.config.annotation.CorsRegistry;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//
-//@Configuration
-//public class WebConfig implements WebMvcConfigurer {
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping("/**")
-//                .allowedOrigins("*") // 허용할 출처
-//                .allowedHeaders("*")
-//                .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS" , "PATCH")
-//                .exposedHeaders("Authorization");
-//    }
-//}
+package com.DevEx.DevExBE.global.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*") // 허용할 출처
+                .allowedHeaders("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS" , "PATCH")
+                .exposedHeaders("Authorization");
+    }
+}
