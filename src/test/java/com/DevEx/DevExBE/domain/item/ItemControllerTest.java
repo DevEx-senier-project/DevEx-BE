2a35ead71158952097ba8d7b527110c86110984a
YASICJUNWOO <joonoo3@naver.com>
2023-11-20 03:20:51 +0900

    test: 전체 아이템 조회 테스트

--- src/test/java/com/DevEx/DevExBE/domain/item/ItemControllerTest.java
+++ src/test/java/com/DevEx/DevExBE/domain/item/ItemControllerTest.java
@@ -1,6 +1,7 @@
 package com.DevEx.DevExBE.domain.item;
 
 import com.DevEx.DevExBE.domain.item.dto.ItemRequestDto;
+import com.DevEx.DevExBE.util.Convertor;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import org.assertj.core.api.Assertions;
@@ -11,10 +12,12 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.context.WebApplicationContext;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
@@ -50,8 +53,28 @@ class ItemControllerTest {
                 .andReturn().getResponse().getStatus();
 
         //then
-        Assertions.assertThat(status).isEqualTo(200);
+        assertThat(status).isEqualTo(200);
 
+    }
+
+    @Test
+    void 아이템_전체_리스트_조회() throws Exception {
+
+        //given
+
+        //when
+        int status = mockMvc.perform(get("/api/Item")
+                        .contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andReturn().getResponse().getStatus();
+
+        //then
+        assertThat(status).isEqualTo(200);
+
+    }
+
+    // TODO: 2023-11-19 [공준우] 테스트 코드 작성
+    @Test
+    void deleteItem() {
     }
 
 }
