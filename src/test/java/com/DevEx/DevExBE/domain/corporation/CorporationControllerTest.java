3911f137566e023843da423bbd60e1a662ceba70
YASICJUNWOO <joonoo3@naver.com>
2023-11-20 03:27:46 +0900

    test: 회사 등록 조호 예외 테스트

--- src/test/java/com/DevEx/DevExBE/domain/corporation/CorporationControllerTest.java
+++ src/test/java/com/DevEx/DevExBE/domain/corporation/CorporationControllerTest.java
@@ -1,9 +1,9 @@
 package com.DevEx.DevExBE.domain.corporation;
 
 import com.DevEx.DevExBE.domain.corporation.dto.CorporationRequestDto;
+import com.DevEx.DevExBE.util.Convertor;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,10 +11,12 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.context.WebApplicationContext;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
@@ -54,7 +56,7 @@ class CorporationControllerTest {
                 .andReturn().getResponse().getStatus();
 
         //then
-        Assertions.assertThat(status).isEqualTo(201);
+        assertThat(status).isEqualTo(201);
     }
 
     @Test
@@ -67,7 +69,7 @@ class CorporationControllerTest {
                 .andReturn().getResponse().getStatus();
 
         //then
-        Assertions.assertThat(status).isEqualTo(200);
+        assertThat(status).isEqualTo(200);
     }
 
     @Test
@@ -80,7 +82,51 @@ class CorporationControllerTest {
                 .andReturn().getResponse().getStatus();
 
         //then
-        Assertions.assertThat(status).isEqualTo(200);
+        assertThat(status).isEqualTo(200);
+
+    }
+
+    @Test
+    void 중복_회사_등록() throws Exception {
+
+        //given
+        CorporationRequestDto corporationRequestDto = CorporationRequestDto.builder()
+                .corpName("testCorpName")
+                .email("testEmail")
+                .tell("testTell")
+                .build();
+
+        String json = gson.toJson(corporationRequestDto);
+
+        MockHttpServletRequestBuilder post = post("/api/corporation")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json);
+
+        mockMvc.perform(post);
+
+        //when
+        String contentAsString = mockMvc.perform(post)
+                .andReturn().getResponse().getContentAsString();
+
+        String code = Convertor.getCode(Convertor.stringToJsonObject(contentAsString));
+
+        //then
+        assertThat(code).isEqualTo("COR_001");
+    }
+
+    @Test
+    void 없는_회사_조회() throws Exception {
+
+        //given
+
+        //when
+        String contentAsString = mockMvc.perform(get("/api/corporation/1000"))
+                .andReturn().getResponse().getContentAsString();
+
+        String code = Convertor.getCode(Convertor.stringToJsonObject(contentAsString));
+
+        //then
+        assertThat(code).isEqualTo("COR_002");
 
     }
 }
