917a865e3a96f53ceac8c09d58a9b630683b9fb6
tjdgns8439 <tjdgns8439@inha.edu>
2023-11-21 19:32:46 +0900

    fix: update getUserId
    - id로 user 조회 시 존재하지 않으면 서비스 단에서 예외처리

--- src/main/java/com/DevEx/DevExBE/domain/users/UserService.java
+++ src/main/java/com/DevEx/DevExBE/domain/users/UserService.java
@@ -2,11 +2,13 @@ package com.DevEx.DevExBE.domain.users;
 
 import com.DevEx.DevExBE.domain.corporation.CorporationRepository;
 import com.DevEx.DevExBE.global.exception.user.UserNotFoundException;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.beans.Transient;
 import java.util.List;
 import java.util.Optional;
 
@@ -16,19 +18,23 @@ public class UserService {
     private final UserRepository userRepository;
     private final CorporationRepository corporationRepository;
 
+    @Transactional
     public List<Users> getUserList() {
         return userRepository.findAll();
     }
 
-    public Optional<Users> getUserId(Long id) {
-        return userRepository.findById(id);
+    @Transactional
+    public Users getUserId(Long id) {
+        return userRepository.findById(id).orElseThrow(UserNotFoundException::new);
     }
 
+    @Transactional
     public void deleteUser(Long id) {
         userRepository.deleteById(id);
     }
 
     // email을 통해 유저 정보를 가져온다.
+    @Transactional
     public Users getUserByEmail(String email) {
         return userRepository.findByEmail(email).orElseThrow(UserNotFoundException::new);
     }
