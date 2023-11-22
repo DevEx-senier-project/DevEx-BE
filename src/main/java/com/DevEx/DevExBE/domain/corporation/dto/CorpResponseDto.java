e009e27d0d3fa4fd7507d0a9ca22df748f418a2f
YASICJUNWOO <joonoo3@naver.com>
2023-11-22 21:03:30 +0900

    feat: 회사 반환 Dto 추가

---
+++ src/main/java/com/DevEx/DevExBE/domain/corporation/dto/CorpResponseDto.java
@@ -0,0 +1,35 @@
+package com.DevEx.DevExBE.domain.corporation.dto;
+
+import com.DevEx.DevExBE.domain.corporation.Corporation;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Builder
+public class CorpResponseDto {
+
+    private String corpName;
+    private String tell;
+    private String email;
+
+    public static CorpResponseDto toDto(Corporation corporation) {
+        return CorpResponseDto.builder()
+                .corpName(corporation.getCorpName())
+                .tell(corporation.getTell())
+                .email(corporation.getEmail())
+                .build();
+    }
+
+    public static CorpResponseDto toFedexDto() {
+        return CorpResponseDto.builder()
+                .corpName("Fedex")
+                .tell("000-0000-0000")
+                .email("Fedex@email.com")
+                .build();
+    }
+
+}
