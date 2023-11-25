8e24aaa0226c875cb41e1aeb81014bf95c49bc7b
YASICJUNWOO <joonoo3@naver.com>
2023-11-26 03:35:34 +0900

    ci: action

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
-BUILD_JAR=$(ls /home/ubuntu/DevExServerProject/build/libs/*.jar)
+BUILD_JAR=$(ls /home/ubuntu/action/build/libs/*.jar)
 JAR_NAME=$(basename $BUILD_JAR)
 TIME_NOW=$(date +%c)
-echo ">>> build 파일명: $JAR_NAME" >> /home/ubuntu/DevExServerProject/deploytest.log
+echo ">>> build 파일명: $JAR_NAME" >> /home/ubuntu/action/deploytest.log
 
-echo ">>> build 파일 복사" >> /home/ec2-ubuntu/DevExServerProject/deploytest.log
-DEPLOY_PATH=/home/ubuntu/DevExServerProject/
+echo ">>> build 파일 복사" >> /home/ec2-ubuntu/action/deploytest.log
+DEPLOY_PATH=/home/ubuntu/action/
 cp $BUILD_JAR $DEPLOY_PATH
 
-echo ">>> 현재 실행중인 애플리케이션 pid 확인" >> /home/ubuntu/DevExServerProject/deploytest.log
+echo ">>> 현재 실행중인 애플리케이션 pid 확인" >> /home/ubuntu/action/deploytest.log
 CURRENT_PID=$(pgrep -f $JAR_NAME)
 
 if [ -z $CURRENT_PID ]
 then
-  echo ">>> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다." >> /home/ubuntu/DevExServerProject/deploytest.log
+  echo ">>> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다." >> /home/ubuntu/action/deploytest.log
 else
   echo ">>> kill -15 $CURRENT_PID"
   kill -15 $CURRENT_PID
@@ -21,6 +21,6 @@ else
 fi
 
 DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
-echo "$TIME_NOW>>> DEPLOY_JAR 배포"    >> /home/ubuntu/DevExServerProject/deploytest.log
-echo "$TIME_NOWv>>> DEPLOY_JAR 주소 :   $DEPLOY_JAR "  >> /home/ubuntu/DevExServerProject/deploytest.log
-nohup java -jar $DEPLOY_JAR > /home/DevExServerProject/deploytest.log 2>/home/ubuntu/DevExServerProject/deploy_err.log &
+echo "$TIME_NOW>>> DEPLOY_JAR 배포"    >> /home/ubuntu/action/deploytest.log
+echo "$TIME_NOWv>>> DEPLOY_JAR 주소 :   $DEPLOY_JAR "  >> /home/ubuntu/action/deploytest.log
+nohup java -jar $DEPLOY_JAR > /home/ubuntu/action/deploytest.log 2>/home/ubuntu/action/deploy_err.log &
