6a0f82fcf7c469119f2d041a4b419e65c2f8c2c1
YASICJUNWOO <joonoo3@naver.com>
2023-11-26 02:13:34 +0900

    feat: deploy 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 BUILD_JAR=$(ls /home/ubuntu/DevExServerProject/build/libs/*.jar)
 JAR_NAME=$(basename $BUILD_JAR)
-echo ">>> build 파일명: $JAR_NAME" >> /home/ubuntu/action/deploy.log
+echo ">>> build 파일명: $JAR_NAME" >> /home/ubuntu/DevExServerProject/deploytest.log
 
-echo ">>> build 파일 복사" >> /home/ec2-ubuntu/action/deploy.log
-DEPLOY_PATH=/home/ubuntu/action/
+echo ">>> build 파일 복사" >> /home/ec2-ubuntu/DevExServerProject/deploytest.log
+DEPLOY_PATH=/home/ubuntu/DevExServerProject/
 cp $BUILD_JAR $DEPLOY_PATH
 
-echo ">>> 현재 실행중인 애플리케이션 pid 확인" >> /home/ubuntu/action/deploy.log
+echo ">>> 현재 실행중인 애플리케이션 pid 확인" >> /home/ubuntu/DevExServerProject/deploytest.log
 CURRENT_PID=$(pgrep -f $JAR_NAME)
 
 if [ -z $CURRENT_PID ]
 then
-  echo ">>> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다." >> /home/ubuntu/action/deploy.log
+  echo ">>> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다." >> /home/ubuntu/DevExServerProject/deploytest.log
 else
   echo ">>> kill -15 $CURRENT_PID"
   kill -15 $CURRENT_PID
@@ -20,5 +20,5 @@ else
 fi
 
 DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
-echo ">>> DEPLOY_JAR 배포"    >> /home/ubuntu/action/deploy.log
-nohup java -jar $DEPLOY_JAR >> /home/ubuntu/deploy.log 2>/home/ubuntu/action/deploy_err.log &
+echo ">>> DEPLOY_JAR 배포"    >> /home/ubuntu/DevExServerProject/deploytest.log
+nohup java -jar $DEPLOY_JAR >> /home/DevExServerProject/deploytest.log 2>/home/ubuntu/DevExServerProject/deploy_err.log &
