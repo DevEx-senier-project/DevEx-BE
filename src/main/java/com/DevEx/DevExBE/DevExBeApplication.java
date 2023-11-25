d7a1d974f54423dbe017a6f0356181c90922141b
tjdgns8439 <tjdgns8439@inha.edu>
2023-11-25 19:24:59 +0900

    fix: delete env

--- src/main/java/com/DevEx/DevExBE/DevExBeApplication.java
+++ src/main/java/com/DevEx/DevExBE/DevExBeApplication.java
@@ -1,16 +1,9 @@
 package com.DevEx.DevExBE;
 
-import env.EnvConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
 @SpringBootApplication
-@PropertySource(value = {
-		"classpath:env/env.yml"
-}, factory = EnvConfig.class)
 public class DevExBeApplication {
 
 	public static void main(String[] args) {
