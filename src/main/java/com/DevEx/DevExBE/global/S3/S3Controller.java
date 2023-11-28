4eb1597439e5ee4dc4d5afa3b95291b95f4eca64
tjdgns8439 <tjdgns8439@inha.edu>
2023-11-28 12:38:59 +0900

    fix: BucketDir.Image에서 지정해준 dir로 찾아가게 변경

--- src/main/java/com/DevEx/DevExBE/global/S3/S3Controller.java
+++ src/main/java/com/DevEx/DevExBE/global/S3/S3Controller.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.function.ToDoubleBiFunction;
 
 @RestController
 @RequestMapping("/S3")
@@ -12,11 +13,13 @@ import java.io.IOException;
 public class S3Controller {
     private final S3Service s3Service;
 
+    // TODO: 2023/11/28 주어진 버킷이름 없을 시 예외 처리 
     @PostMapping
-    public String uploadFile(@RequestParam("file") MultipartFile file) throws IOException {
-        BucketDir bucketDir = BucketDir.Image;
+    public String uploadFile(@RequestParam("file") MultipartFile file, @RequestParam("bucket") String bucket) throws IOException {
+        BucketDir bucketDir = BucketDir.valueOf(bucket);
         return s3Service.uploadFile(bucketDir, file);
     }
+
 
     // TODO: 2023/11/27 파일 이름 지정, 파일 이름으로 찾아오기(클라이언트에게 전달)
 }
