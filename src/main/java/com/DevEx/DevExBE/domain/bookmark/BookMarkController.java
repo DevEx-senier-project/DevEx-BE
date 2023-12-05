9f7774a6904f8181ab606626c26d8c38bb7b6086
YASICJUNWOO <joonoo3@naver.com>
2023-12-05 18:36:26 +0900

    feat : 북마크 삭제

--- src/main/java/com/DevEx/DevExBE/domain/bookmark/BookMarkController.java
+++ src/main/java/com/DevEx/DevExBE/domain/bookmark/BookMarkController.java
@@ -8,10 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/bookmark")
@@ -23,9 +20,16 @@ public class BookMarkController {
 
     @Operation(summary = "북마크 생성", description = "북마크 생성")
     @PostMapping
-    public ResponseEntity<?> createBookMark(@AuthenticationPrincipal UserDetails user, @RequestBody UserQuoteRequestDto userQuoteRequestDto){
+    public ResponseEntity<Long> createBookMark(@AuthenticationPrincipal UserDetails user, @RequestBody UserQuoteRequestDto userQuoteRequestDto){
         System.out.println(userQuoteRequestDto);
-        bookMarkService.createBookMark(user, userQuoteRequestDto);
-        return ResponseEntity.status(HttpStatus.CREATED).build();
+        Long bookMarkId = bookMarkService.createBookMark(user, userQuoteRequestDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(bookMarkId);
+    }
+
+    @Operation(summary = "북마크 삭제", description = "북마크 삭제")
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Long> createBookMark(@PathVariable Long id){
+        bookMarkService.deleteBookMark(id);
+        return ResponseEntity.status(HttpStatus.OK).body(id);
     }
 }
--- src/main/java/com/DevEx/DevExBE/domain/bookmark/BookMarkService.java
+++ src/main/java/com/DevEx/DevExBE/domain/bookmark/BookMarkService.java
@@ -14,10 +14,14 @@ public class BookMarkService {
     private final BookMarkRepository bookMarkRepository;
     private final UserService userService;
 
-    public void createBookMark(UserDetails user, UserQuoteRequestDto userQuoteRequestDto) {
+    public Long createBookMark(UserDetails user, UserQuoteRequestDto userQuoteRequestDto) {
         Users userEntity = userService.getUserEntityByEmail(user.getUsername());
         BookMark bookMarkEntity = BookMark.toEntity(userEntity, userQuoteRequestDto);
-        System.out.println(bookMarkEntity);
-        bookMarkRepository.save(bookMarkEntity);
+        BookMark savedBookMark = bookMarkRepository.save(bookMarkEntity);
+        return savedBookMark.getId();
+    }
+
+    public void deleteBookMark(Long id) {
+        bookMarkRepository.deleteById(id);
     }
 }
